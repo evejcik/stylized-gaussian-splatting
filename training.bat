67a580df35c772fa463b409b2e46f18272f1232b
Emma Vejcik <47393221+evejcik@users.noreply.github.com>
2025-02-13 16:04:05 -0500

    adding training.bat file

---
+++ training.bat
@@ -0,0 +1,67 @@
+
+
+python train.py -s data/ferns --checkpoint_iterations 31000 32000 33000 --save_iterations 31000 32000 33000 --style_path style/s5.jpeg --stylize_iter_start 30001 --data_device gpu --iterations 33000 --start_checkpoint preReconScenes/ferns_hq/chkpnt30000.pth --model_path output/ferns/s5
+
+python train.py -s data/ferns --checkpoint_iterations 31000 32000 33000 --save_iterations 31000 32000 33000 --style_path style/watering_cam.jpg --stylize_iter_start 30001 --data_device gpu --iterations 33000 --start_checkpoint preReconScenes/ferns_hq/chkpnt30000.pth --model_path output/ferns/watering_cam
+
+
+python train.py -s data/ferns --checkpoint_iterations 31000 32000 33000 --save_iterations 31000 32000 33000 --style_path style/055.jpg --stylize_iter_start 30001 --data_device gpu --iterations 33000 --start_checkpoint preReconScenes/ferns_hq/chkpnt30000.pth --model_path output/ferns/055
+
+python train.py -s data/ferns --checkpoint_iterations 31000 32000 33000 --save_iterations 31000 32000 33000 --style_path style/closed_eyes.jpg --stylize_iter_start 30001 --data_device gpu --iterations 33000 --start_checkpoint preReconScenes/ferns_hq/chkpnt30000.pth --model_path output/ferns/closed_eyes
+
+python train.py -s data/ferns --checkpoint_iterations 31000 32000 33000 --save_iterations 31000 32000 33000 --style_path style/night_landscape.jpg --stylize_iter_start 30001 --data_device gpu --iterations 33000 --start_checkpoint preReconScenes/ferns_hq/chkpnt30000.pth --model_path output/ferns/night_landscape
+
+python train.py -s data/ferns --checkpoint_iterations 31000 32000 33000 --save_iterations 31000 32000 33000 --style_path style/Piet_Mondrian_Bauhaus.jpg --stylize_iter_start 30001 --data_device gpu --iterations 33000 --start_checkpoint preReconScenes/ferns_hq/chkpnt30000.pth --model_path output/ferns/piet_mondrian
+
+python train.py -s data/ferns --checkpoint_iterations 31000 32000 33000 --save_iterations 31000 32000 33000 --style_path style/post_apocalyptic.jpg --stylize_iter_start 30001 --data_device gpu --iterations 33000 --start_checkpoint preReconScenes/ferns_hq/chkpnt30000.pth --model_path output/fern/post_apocalyptic
+
+python train.py -s data/ferns --checkpoint_iterations 31000 32000 33000 --save_iterations 31000 32000 33000 --style_path style/s1.png --stylize_iter_start 30001 --data_device gpu --iterations 33000 --start_checkpoint preReconScenes/ferns_hq/chkpnt30000.pth --model_path output/ferns/s1
+
+python train.py -s data/ferns --checkpoint_iterations 31000 32000 33000 --save_iterations 31000 32000 33000 --style_path style/s2.png --stylize_iter_start 30001 --data_device gpu --iterations 33000 --start_checkpoint preReconScenes/ferns_hq/chkpnt30000.pth --model_path output/ferns/s2
+
+python train.py -s data/ferns --checkpoint_iterations 31000 32000 33000 --save_iterations 31000 32000 33000 --style_path style/s3.jpeg --stylize_iter_start 30001 --data_device gpu --iterations 33000 --start_checkpoint preReconScenes/ferns_hq/chkpnt30000.pth --model_path output/ferns/s3
+
+python train.py -s data/ferns --checkpoint_iterations 31000 32000 33000 --save_iterations 31000 32000 33000 --style_path style/s4.jpeg --stylize_iter_start 30001 --data_device gpu --iterations 33000 --start_checkpoint preReconScenes/ferns_hq/chkpnt30000.pth --model_path output/ferns_s5
+
+
+python train.py -s data/ferns --checkpoint_iterations 31000 32000 33000 --save_iterations 31000 32000 33000 --style_path style/s5.jpeg --stylize_iter_start 30001 --data_device gpu --iterations 33000 --start_checkpoint preReconScenes/ferns_hq/chkpnt30000.pth --model_path output/ferns/s5
+
+python train.py -s data/ferns --checkpoint_iterations 31000 32000 33000 --save_iterations 31000 32000 33000 --style_path style/seated_nude.jpg --stylize_iter_start 30001 --data_device gpu --iterations 33000 --start_checkpoint preReconScenes/ferns_hq/chkpnt30000.pth --model_path output/ferns/seated_nude
+
+python train.py -s data/ferns --checkpoint_iterations 31000 32000 33000 --save_iterations 31000 32000 33000 --style_path style/starry_night.jpg --stylize_iter_start 30001 --data_device gpu --iterations 33000 --start_checkpoint preReconScenes/ferns_hq/chkpnt30000.pth --model_path output/ferns/starry_night
+
+
+
+
+python train.py -s data/bicycle --checkpoint_iterations 31000 32000 33000 --save_iterations 31000 32000 33000 --style_path style/s5.jpeg --stylize_iter_start 30001 --data_device gpu --iterations 33000 --start_checkpoint preReconScenes/bicycle/chkpnt30000.pth --model_path output/bicycle/s5
+
+python train.py -s data/bicycle --checkpoint_iterations 31000 32000 33000 --save_iterations 31000 32000 33000 --style_path style/watering_cam.jpg --stylize_iter_start 30001 --data_device gpu --iterations 33000 --start_checkpoint preReconScenes/bicycle/chkpnt30000.pth --model_path output/bicycle/watering_cam
+
+
+python train.py -s data/bicycle --checkpoint_iterations 31000 32000 33000 --save_iterations 31000 32000 33000 --style_path style/055.jpg --stylize_iter_start 30001 --data_device gpu --iterations 33000 --start_checkpoint preReconScenes/bicycle/chkpnt30000.pth --model_path output/bicycle/055
+
+python train.py -s data/bicycle --checkpoint_iterations 31000 32000 33000 --save_iterations 31000 32000 33000 --style_path style/closed_eyes.jpg --stylize_iter_start 30001 --data_device gpu --iterations 33000 --start_checkpoint preReconScenes/bicycle/chkpnt30000.pth --model_path output/bicycle/closed_eyes
+
+python train.py -s data/bicycle --checkpoint_iterations 31000 32000 33000 --save_iterations 31000 32000 33000 --style_path style/night_landscape.jpg --stylize_iter_start 30001 --data_device gpu --iterations 33000 --start_checkpoint preReconScenes/bicycle/chkpnt30000.pth --model_path output/bicycle/night_landscape
+
+python train.py -s data/bicycle --checkpoint_iterations 31000 32000 33000 --save_iterations 31000 32000 33000 --style_path style/Piet_Mondrian_Bauhaus.jpg --stylize_iter_start 30001 --data_device gpu --iterations 33000 --start_checkpoint preReconScenes/bicycle/chkpnt30000.pth --model_path output/bicycle/piet_mondrian
+
+python train.py -s data/bicycle --checkpoint_iterations 31000 32000 33000 --save_iterations 31000 32000 33000 --style_path style/post_apocalyptic.jpg --stylize_iter_start 30001 --data_device gpu --iterations 33000 --start_checkpoint preReconScenes/bicycle/chkpnt30000.pth --model_path output/fern/post_apocalyptic
+
+python train.py -s data/bicycle --checkpoint_iterations 31000 32000 33000 --save_iterations 31000 32000 33000 --style_path style/s1.png --stylize_iter_start 30001 --data_device gpu --iterations 33000 --start_checkpoint preReconScenes/bicycle/chkpnt30000.pth --model_path output/bicycle/s1
+
+python train.py -s data/bicycle --checkpoint_iterations 31000 32000 33000 --save_iterations 31000 32000 33000 --style_path style/s2.png --stylize_iter_start 30001 --data_device gpu --iterations 33000 --start_checkpoint preReconScenes/bicycle/chkpnt30000.pth --model_path output/bicycle/s2
+
+python train.py -s data/bicycle --checkpoint_iterations 31000 32000 33000 --save_iterations 31000 32000 33000 --style_path style/s3.jpeg --stylize_iter_start 30001 --data_device gpu --iterations 33000 --start_checkpoint preReconScenes/bicycle/chkpnt30000.pth --model_path output/bicycle/s3
+
+python train.py -s data/bicycle --checkpoint_iterations 31000 32000 33000 --save_iterations 31000 32000 33000 --style_path style/s4.jpeg --stylize_iter_start 30001 --data_device gpu --iterations 33000 --start_checkpoint preReconScenes/bicycle/chkpnt30000.pth --model_path output/bicycle/s4
+
+
+python train.py -s data/bicycle --checkpoint_iterations 31000 32000 33000 --save_iterations 31000 32000 33000 --style_path style/s5.jpeg --stylize_iter_start 30001 --data_device gpu --iterations 33000 --start_checkpoint preReconScenes/bicycle/chkpnt30000.pth --model_path output/bicycle/s5
+
+python train.py -s data/bicycle --checkpoint_iterations 31000 32000 33000 --save_iterations 31000 32000 33000 --style_path style/seated_nude.jpg --stylize_iter_start 30001 --data_device gpu --iterations 33000 --start_checkpoint preReconScenes/bicycle/chkpnt30000.pth --model_path output/bicycle/seated_nude
+
+python train.py -s data/bicycle --checkpoint_iterations 31000 32000 33000 --save_iterations 31000 32000 33000 --style_path style/starry_night.jpg --stylize_iter_start 30001 --data_device gpu --iterations 33000 --start_checkpoint preReconScenes/bicycle/chkpnt30000.pth --model_path output/bicycle/starry_night
+
+
+
