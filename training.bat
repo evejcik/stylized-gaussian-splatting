bafcac6b3e556bc327e99aabfc61aefc2cf43ef2
Emma Vejcik <47393221+evejcik@users.noreply.github.com>
2025-02-13 16:13:41 -0500

    adding training.bat file full

--- training.bat
+++ training.bat
@@ -65,3 +65,73 @@ python train.py -s data/bicycle --checkpoint_iterations 31000 32000 33000 --save
 
 
 
+
+python train.py -s data/flower --checkpoint_iterations 31000 32000 33000 --save_iterations 31000 32000 33000 --style_path style/s5.jpeg --stylize_iter_start 30001 --data_device gpu --iterations 33000 --start_checkpoint preReconScenes/flower_hq/chkpnt30000.pth --model_path output/flower/s5
+
+python train.py -s data/flower --checkpoint_iterations 31000 32000 33000 --save_iterations 31000 32000 33000 --style_path style/watering_cam.jpg --stylize_iter_start 30001 --data_device gpu --iterations 33000 --start_checkpoint preReconScenes/flower_hq/chkpnt30000.pth --model_path output/flower/watering_cam
+
+
+python train.py -s data/flower --checkpoint_iterations 31000 32000 33000 --save_iterations 31000 32000 33000 --style_path style/055.jpg --stylize_iter_start 30001 --data_device gpu --iterations 33000 --start_checkpoint preReconScenes/flower_hq/chkpnt30000.pth --model_path output/flower/055
+
+python train.py -s data/flower --checkpoint_iterations 31000 32000 33000 --save_iterations 31000 32000 33000 --style_path style/closed_eyes.jpg --stylize_iter_start 30001 --data_device gpu --iterations 33000 --start_checkpoint preReconScenes/flower_hq/chkpnt30000.pth --model_path output/flower/closed_eyes
+
+python train.py -s data/flower --checkpoint_iterations 31000 32000 33000 --save_iterations 31000 32000 33000 --style_path style/night_landscape.jpg --stylize_iter_start 30001 --data_device gpu --iterations 33000 --start_checkpoint preReconScenes/flower_hq/chkpnt30000.pth --model_path output/flower/night_landscape
+
+python train.py -s data/flower --checkpoint_iterations 31000 32000 33000 --save_iterations 31000 32000 33000 --style_path style/Piet_Mondrian_Bauhaus.jpg --stylize_iter_start 30001 --data_device gpu --iterations 33000 --start_checkpoint preReconScenes/flower_hq/chkpnt30000.pth --model_path output/flower/piet_mondrian
+
+python train.py -s data/flower --checkpoint_iterations 31000 32000 33000 --save_iterations 31000 32000 33000 --style_path style/post_apocalyptic.jpg --stylize_iter_start 30001 --data_device gpu --iterations 33000 --start_checkpoint preReconScenes/flower_hq/chkpnt30000.pth --model_path output/fern/post_apocalyptic
+
+python train.py -s data/flower --checkpoint_iterations 31000 32000 33000 --save_iterations 31000 32000 33000 --style_path style/s1.png --stylize_iter_start 30001 --data_device gpu --iterations 33000 --start_checkpoint preReconScenes/flower_hq/chkpnt30000.pth --model_path output/flower/s1
+
+python train.py -s data/flower --checkpoint_iterations 31000 32000 33000 --save_iterations 31000 32000 33000 --style_path style/s2.png --stylize_iter_start 30001 --data_device gpu --iterations 33000 --start_checkpoint preReconScenes/flower_hq/chkpnt30000.pth --model_path output/flower/s2
+
+python train.py -s data/flower --checkpoint_iterations 31000 32000 33000 --save_iterations 31000 32000 33000 --style_path style/s3.jpeg --stylize_iter_start 30001 --data_device gpu --iterations 33000 --start_checkpoint preReconScenes/flower_hq/chkpnt30000.pth --model_path output/flower/s3
+
+python train.py -s data/flower --checkpoint_iterations 31000 32000 33000 --save_iterations 31000 32000 33000 --style_path style/s4.jpeg --stylize_iter_start 30001 --data_device gpu --iterations 33000 --start_checkpoint preReconScenes/flower_hq/chkpnt30000.pth --model_path output/flower/s4
+
+
+python train.py -s data/flower --checkpoint_iterations 31000 32000 33000 --save_iterations 31000 32000 33000 --style_path style/s5.jpeg --stylize_iter_start 30001 --data_device gpu --iterations 33000 --start_checkpoint preReconScenes/flower_hq/chkpnt30000.pth --model_path output/flower/s5
+
+python train.py -s data/flower --checkpoint_iterations 31000 32000 33000 --save_iterations 31000 32000 33000 --style_path style/seated_nude.jpg --stylize_iter_start 30001 --data_device gpu --iterations 33000 --start_checkpoint preReconScenes/flower_hq/chkpnt30000.pth --model_path output/flower/seated_nude
+
+python train.py -s data/flower --checkpoint_iterations 31000 32000 33000 --save_iterations 31000 32000 33000 --style_path style/starry_night.jpg --stylize_iter_start 30001 --data_device gpu --iterations 33000 --start_checkpoint preReconScenes/flower_hq/chkpnt30000.pth --model_path output/flower/starry_night
+
+
+
+
+python train.py -s data/trottier --checkpoint_iterations 31000 32000 33000 --save_iterations 31000 32000 33000 --style_path style/s5.jpeg --stylize_iter_start 30001 --data_device gpu --iterations 33000 --start_checkpoint preReconScenes/trottier/chkpnt30000.pth --model_path output/trottier/s5
+
+python train.py -s data/trottier --checkpoint_iterations 31000 32000 33000 --save_iterations 31000 32000 33000 --style_path style/watering_cam.jpg --stylize_iter_start 30001 --data_device gpu --iterations 33000 --start_checkpoint preReconScenes/trottier/chkpnt30000.pth --model_path output/trottier/watering_cam
+
+
+python train.py -s data/trottier --checkpoint_iterations 31000 32000 33000 --save_iterations 31000 32000 33000 --style_path style/055.jpg --stylize_iter_start 30001 --data_device gpu --iterations 33000 --start_checkpoint preReconScenes/trottier/chkpnt30000.pth --model_path output/trottier/055
+
+python train.py -s data/trottier --checkpoint_iterations 31000 32000 33000 --save_iterations 31000 32000 33000 --style_path style/closed_eyes.jpg --stylize_iter_start 30001 --data_device gpu --iterations 33000 --start_checkpoint preReconScenes/trottier/chkpnt30000.pth --model_path output/trottier/closed_eyes
+
+python train.py -s data/trottier --checkpoint_iterations 31000 32000 33000 --save_iterations 31000 32000 33000 --style_path style/night_landscape.jpg --stylize_iter_start 30001 --data_device gpu --iterations 33000 --start_checkpoint preReconScenes/trottier/chkpnt30000.pth --model_path output/trottier/night_landscape
+
+python train.py -s data/trottier --checkpoint_iterations 31000 32000 33000 --save_iterations 31000 32000 33000 --style_path style/Piet_Mondrian_Bauhaus.jpg --stylize_iter_start 30001 --data_device gpu --iterations 33000 --start_checkpoint preReconScenes/trottier/chkpnt30000.pth --model_path output/trottier/piet_mondrian
+
+python train.py -s data/trottier --checkpoint_iterations 31000 32000 33000 --save_iterations 31000 32000 33000 --style_path style/post_apocalyptic.jpg --stylize_iter_start 30001 --data_device gpu --iterations 33000 --start_checkpoint preReconScenes/trottier/chkpnt30000.pth --model_path output/fern/post_apocalyptic
+
+python train.py -s data/trottier --checkpoint_iterations 31000 32000 33000 --save_iterations 31000 32000 33000 --style_path style/s1.png --stylize_iter_start 30001 --data_device gpu --iterations 33000 --start_checkpoint preReconScenes/trottier/chkpnt30000.pth --model_path output/trottier/s1
+
+python train.py -s data/trottier --checkpoint_iterations 31000 32000 33000 --save_iterations 31000 32000 33000 --style_path style/s2.png --stylize_iter_start 30001 --data_device gpu --iterations 33000 --start_checkpoint preReconScenes/trottier/chkpnt30000.pth --model_path output/trottier/s2
+
+python train.py -s data/trottier --checkpoint_iterations 31000 32000 33000 --save_iterations 31000 32000 33000 --style_path style/s3.jpeg --stylize_iter_start 30001 --data_device gpu --iterations 33000 --start_checkpoint preReconScenes/trottier/chkpnt30000.pth --model_path output/trottier/s3
+
+python train.py -s data/trottier --checkpoint_iterations 31000 32000 33000 --save_iterations 31000 32000 33000 --style_path style/s4.jpeg --stylize_iter_start 30001 --data_device gpu --iterations 33000 --start_checkpoint preReconScenes/trottier/chkpnt30000.pth --model_path output/trottier/s4
+
+
+python train.py -s data/trottier --checkpoint_iterations 31000 32000 33000 --save_iterations 31000 32000 33000 --style_path style/s5.jpeg --stylize_iter_start 30001 --data_device gpu --iterations 33000 --start_checkpoint preReconScenes/trottier/chkpnt30000.pth --model_path output/trottier/s5
+
+python train.py -s data/trottier --checkpoint_iterations 31000 32000 33000 --save_iterations 31000 32000 33000 --style_path style/seated_nude.jpg --stylize_iter_start 30001 --data_device gpu --iterations 33000 --start_checkpoint preReconScenes/trottier/chkpnt30000.pth --model_path output/trottier/seated_nude
+
+python train.py -s data/trottier --checkpoint_iterations 31000 32000 33000 --save_iterations 31000 32000 33000 --style_path style/starry_night.jpg --stylize_iter_start 30001 --data_device gpu --iterations 33000 --start_checkpoint preReconScenes/trottier/chkpnt30000.pth --model_path output/trottier/starry_night
+
+
+
+
+
+
+
